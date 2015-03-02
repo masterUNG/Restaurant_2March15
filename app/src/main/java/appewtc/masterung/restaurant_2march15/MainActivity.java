ccb559223eebb0b82bad47812f8aa2df9317a3ad
masterUNG <phrombutr@gmail.com>
2015-03-02 09:55:24 +0700

    Start Open

--- app/src/main/java/appewtc/masterung/restaurant_2march15/MainActivity.java
+++ app/src/main/java/appewtc/masterung/restaurant_2march15/MainActivity.java
@@ -36,4 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/restaurant_2march15/MyOpenHelper.java
@@ -0,0 +1,11 @@
+package appewtc.masterung.restaurant_2march15;
+
+/**
+ * Created by masterUNG on 3/2/15 AD.
+ */
+public class MyOpenHelper {
+
+    public MyOpenHelper() {
+    }   // Constructor
+    
+}   // Main Class
